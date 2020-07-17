018b14d9c6ed610efc92dd4d250abe61ea13d033
kaizer1v <kaizer1v@gmail.com>
2020-07-17 13:52:21 +0530

    reading from config

--- md_reader.py
+++ md_reader.py
@@ -4,22 +4,29 @@ from flask import Flask, render_template
 import yaml
 
 app = Flask(__name__)
-
-POSTS_DIR = 'posts/'
+config = {}
 posts = []
-post_files = [f for f in os.listdir(POSTS_DIR) if os.path.isfile(os.path.join(POSTS_DIR, f))]
 
-for pf in post_files:
-    fname = os.path.splitext(pf)[0]
-    posts.append({
-        'title': ' '.join(fname.split('_')),
-        'url': os.path.join(POSTS_DIR, pf)
-    })
+if os.path.exists('config.yaml'):
+    config = yaml.load(open('config.yaml', 'r'), Loader=yaml.FullLoader)
+    post_files = [f for f in os.listdir(config['POSTS_DIR']) if os.path.isfile(os.path.join(config['POSTS_DIR'], f))]
+
+    for pf in post_files:
+        fname = os.path.splitext(pf)[0]
+        posts.append({
+            'title': ' '.join(fname.split('_')),
+            'url': os.path.join(config['POSTS_DIR'], pf)
+        })
+else:
+    print('ERROR: Flog requires a config.yaml file in the root directory')
 
 
 @app.route('/')
 def all_posts():
-    return render_template('posts.html', posts=posts)
+    return render_template('posts.html', data={
+        'blog_title': config['BLOG_TITLE'],
+        'posts': posts
+    })
 
 
 @app.route('/<path:md_file>')
@@ -27,7 +34,7 @@ def single_post(md_file):
     with open(md_file) as file_handle:
         markdown_data = file_handle.read()
     md = mistune.markdown(markdown_data)
-    return render_template("single_post.html", content=md)
+    return render_template('single_post.html', content=md)
 
 
 @app.errorhandler(500)
